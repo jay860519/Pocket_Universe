c040e7ae98d338c26f31416b40bc44fda0c20037
jay860519 <113287192+jay860519@users.noreply.github.com>
2025-04-23 22:00:02 +0800

    convex polygon version1

--- convex_polygon.cpp
+++ convex_polygon.cpp
@@ -1,1 +1,11 @@
+#include<stdio.h>
+#include<vector>
 
+class convex_polygon{
+    public:
+        vector<pair<float, float>> coordinate;
+};
+
+int main(){
+    
+}
--- main.cpp
+++ main.cpp
@@ -1,6 +1,13 @@
+<<<<<<< HEAD
+=======
 #include <iostream>
+>>>>>>> 62a4a48dfe885aa3586740b1fad790c410af1f27
 
+<<<<<<< HEAD
+=======
 int main() {
     std::cout << "Hello" << std::endl;
     return 0;
 }
+>>>>>>> 62a4a48dfe885aa3586740b1fad790c410af1f27
+
