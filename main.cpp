091c0deeb43ca41a643175c201da25d66e988bff
jay860519 <113287192+jay860519@users.noreply.github.com>
2025-04-23 22:08:57 +0800

    main version 1

--- main.cpp
+++ main.cpp
@@ -1,14 +1,9 @@
-<<<<<<< HEAD
-=======
 #include "convex_polygon.h"
 #include <iostream>
->>>>>>> 62a4a48dfe885aa3586740b1fad790c410af1f27
 
-<<<<<<< HEAD
-=======
 int main() {
     std::cout << "Hello" << std::endl;
     return 0;
 }
->>>>>>> 62a4a48dfe885aa3586740b1fad790c410af1f27
+
 
